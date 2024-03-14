1c1b0bcb06b76379447fde6836dba472dace97ea
Rajeesh Durai Thanga Raja <rajeeshdurai.thangaraja@kanini.com>
2024-03-14 19:14:09 +0530

    Added user repository

---
+++ Service/annex-cloud-auth-jwt/repository/iUserRespository.go
@@ -0,0 +1,15 @@
+package repository
+
+import (
+	"login-jwt/entity"
+	"github.com/google/uuid"
+)
+
+type IUserRepository interface {
+	Save(user entity.User)
+	Update(user entity.User)
+	Delete(userId uuid.UUID)
+	GetById(userId uuid.UUID) (entity.User, error)
+	GetAll() []entity.User
+	GetByEmail(email string) (entity.User, error)
+}
