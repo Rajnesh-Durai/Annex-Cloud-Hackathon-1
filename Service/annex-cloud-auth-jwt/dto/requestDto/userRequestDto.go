c20679904288f2d71a74737136a00fe057275e53
Rajeesh Durai Thanga Raja <rajeeshdurai.thangaraja@kanini.com>
2024-03-14 19:12:47 +0530

    Added new user request and response dto

---
+++ Service/annex-cloud-auth-jwt/dto/requestDto/userRequestDto.go
@@ -0,0 +1,23 @@
+package requestdto
+
+import "github.com/google/uuid"
+
+type CreateUsersRequestDto struct {
+	Username string `validate:"required,min=2,max=50" json:"username"`
+	Email    string `validate:"required,min=2,max=100" json:"email"`
+	Password string `validate:"required,min=2,max=100" json:"password"`
+	Role string `validate:"required,min=2,max=50" json:"role"`
+}
+
+type UpdateUsersRequestDto struct {
+	Id       uuid.UUID    `validate:"required"`
+	Username string `validate:"required,max=50,min=2" json:"username"`
+	Email    string `validate:"required,min=2,max=100" json:"email"`
+	Password string `validate:"required,min=2,max=100" json:"password"`
+	Role string `validate:"required,min=2,max=50" json:"role"`
+}
+
+type LoginRequestDto struct {
+	Email string `validate:"required,max=100,min=2" json:"email"`
+	Password string `validate:"required,min=2,max=100" json:"password"`
+}
