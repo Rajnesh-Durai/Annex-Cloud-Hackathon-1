af23b5b0aa2d746843aac40771e2c39d1248b18d
Vishal Bharath Kumar Thiyagarajan <vishalbharathkumar.thiyagarajan@kanini.com>
2024-03-24 11:33:33 +0530

    Feature: Updated Dashboard Page

--- Client/login-mfe/src/routes/+page.svelte
+++ Client/login-mfe/src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
     import { Router, Route } from 'svelte-routing';
     import Login from '../pages/login.svelte';
+    import Dashboard from '../pages/dashboard.svelte';
 </script>
 
 <Router >
     <Route path="/"><Login/></Route>
+    <Route path="/dashboard"><Dashboard/></Route>
 </Router>
